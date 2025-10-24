14d52badd7f813275a9730ae3f812e4650059176
ba2slk <0730bss@gmail.com>
2025-10-25 00:00:41 +0900

    Initial commit

---
+++ src/test/java/com/team/catchup/CatchupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.catchup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
