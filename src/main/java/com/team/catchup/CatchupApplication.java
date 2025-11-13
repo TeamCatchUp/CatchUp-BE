8c1cb58f72ccd767bc1bfeea09533e037f4e6f96
ba2slk <0730bss@gmail.com>
2025-11-14 02:16:16 +0900

    chore: CatchupApplication import 정리

--- src/main/java/com/team/catchup/CatchupApplication.java
+++ src/main/java/com/team/catchup/CatchupApplication.java
@@ -2,9 +2,6 @@ package com.team.catchup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class CatchupApplication {
