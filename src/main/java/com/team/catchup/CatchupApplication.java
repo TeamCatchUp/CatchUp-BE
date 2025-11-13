5d869d6d33ebc2c7276cd4cfe6bca619922cf248
ba2slk <0730bss@gmail.com>
2025-11-14 02:12:11 +0900

    fix: SpringBootApplication DB, Security 제외 설정 복구

--- src/main/java/com/team/catchup/CatchupApplication.java
+++ src/main/java/com/team/catchup/CatchupApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		SecurityAutoConfiguration.class,
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class CatchupApplication {
 
 	public static void main(String[] args) {
