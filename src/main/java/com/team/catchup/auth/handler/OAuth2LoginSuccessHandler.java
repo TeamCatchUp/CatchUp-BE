26ccce722768ddd744f5ed049d9f96100f84dfbc
ba2slk <0730bss@gmail.com>
2026-01-05 21:46:40 +0900

    feat: cookie 기반 access token 발급

--- src/main/java/com/team/catchup/auth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/team/catchup/auth/handler/OAuth2LoginSuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -36,7 +37,13 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         String accessToken = tokenProvider.createAccessToken(email, role, memberId);
 
-        response.addHeader("Authorization", "Bearer " + accessToken);
+        ResponseCookie cookie = ResponseCookie.from("accessToken", accessToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(7*24*60*60)
+                .sameSite("Lax")
+                .build();
 
         response.sendRedirect(redirectUri); // 프론트엔드 홈화면으로 리다이렉트
     }
