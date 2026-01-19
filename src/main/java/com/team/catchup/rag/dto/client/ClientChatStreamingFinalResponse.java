7a4dce7b911bdeed0f08ff642e7129106948e38a
ba2slk <0730bss@gmail.com>
2026-01-20 03:00:11 +0900

    feat: SSE 응답에 session_id 포함

--- src/main/java/com/team/catchup/rag/dto/client/ClientChatStreamingFinalResponse.java
+++ src/main/java/com/team/catchup/rag/dto/client/ClientChatStreamingFinalResponse.java
@@ -3,11 +3,14 @@ package com.team.catchup.rag.dto.client;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.team.catchup.rag.dto.server.FastApiStreamingResponse;
 
+import java.util.UUID;
+
 /**
  * 답변 생성 과정 Streaming 마지막 응답으로 Client에게 전달할 데이터
  */
 
 public record ClientChatStreamingFinalResponse(
+        UUID sessionId,
         String type,  // result
         String node,  // generate
 
@@ -19,8 +22,10 @@ public record ClientChatStreamingFinalResponse(
             ClientChatResponse response
     ) {
         return new ClientChatStreamingFinalResponse(
+                dto.getSessionId(),
                 dto.getType(),
                 dto.getNode(),
-                response);
+                response
+        );
     }
 }
