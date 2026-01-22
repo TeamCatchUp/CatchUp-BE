e03af9c3af258f4c2fddd0036c4dfeeee343636a
ba2slk <0730bss@gmail.com>
2026-01-23 05:26:57 +0900

    feat: 최신 커밋 조회 쿼리에 Pageable 적용

--- src/main/java/com/team/catchup/github/repository/GithubCommitRepository.java
+++ src/main/java/com/team/catchup/github/repository/GithubCommitRepository.java
@@ -1,7 +1,10 @@
 package com.team.catchup.github.repository;
 
 import com.team.catchup.github.entity.GithubCommit;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,11 @@ public interface GithubCommitRepository extends JpaRepository<GithubCommit, Long
     List<GithubCommit> findAllByShaIn(List<String> shas);
 
     List<GithubCommit> findByRepository_RepositoryIdAndShaIn(Long repositoryId, List<String> shas);
+
+    @Query("SELECT c " +
+            "FROM GithubCommit  c, GithubFileChange fc " +
+            "WHERE c.sha = fc.commitSha " +
+            "AND fc.filePath = :filePath " +
+            "ORDER BY c.authorDate DESC")
+    List<GithubCommit> findLatestByFilePath(@Param("filePath") String filePath, Pageable pageable);
 }
