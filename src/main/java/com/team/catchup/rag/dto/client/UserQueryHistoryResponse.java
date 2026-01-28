63f407099d99ba5c358acb03c2dd9d175763cd85
hxxukii <81512847+gilbert09031@users.noreply.github.com>
2026-01-28 16:12:08 +0900

    fix : 채팅 목록 -> 채팅방 이동용 필드 추가

--- src/main/java/com/team/catchup/rag/dto/client/UserQueryHistoryResponse.java
+++ src/main/java/com/team/catchup/rag/dto/client/UserQueryHistoryResponse.java
@@ -4,14 +4,17 @@ import com.team.catchup.rag.entity.ChatHistory;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Builder
 public record UserQueryHistoryResponse(
+        UUID sessionId,
         String query,
         LocalDateTime createdAt
 ) {
     public static UserQueryHistoryResponse from(ChatHistory history){
         return UserQueryHistoryResponse.builder()
+                .sessionId(history.getChatRoom().getSessionId())
                 .query(history.getContent())
                 .createdAt(history.getCreatedAt())
                 .build();
